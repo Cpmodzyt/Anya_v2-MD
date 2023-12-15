5a0da1ae91c2b19f786a7434e5ca341f66ad166b
Cpmodzyt <121564783+Cpmodzyt@users.noreply.github.com>
2023-12-15 06:08:45 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,32 +1,7 @@
-const { readFileSync, watchFile, unwatchFile } = require('fs')
-const { redBright } = require('chalk')
-require('./lib/config')
+const { readFileSync } = require('fs')
 require("dotenv").config();
 
-global.zApiKey = {// if this API key get expired then please go to https://www.fxacb-api.my.id and get a new api key.
-       one: "zenzkey_998568986d"
-}
-
-
-global.botname = process.env.BotName || "𝙲𝚙'𝚜  𝙱𝚘𝚝  />  ◉" 
-global.author = process.env.Author || "@ItzCp" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
-global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Itz Cp's Bot" 
-global.prefa = process.env.Prefix || ['-'] 
-global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Itz-Cp" 
-global.ownernumber = process.env.Owner_Number || "94765665354" 
-global.adress = process.env.Continent || "Asia, Srilanka, sabaragamwa" 
-global.timezone = process.env.TimeZone || "Asia/colombo" 
-global.instagramId = process.env.Insta || "8.08_only_mine" 
-global.email = process.env.Email_Id || "example@example.com" 
-
-global.mongoUrl = process.env.MongoDB || "Enter"; 
-
-global.warns = process.env.Warns_Limits || 3; 
-
-global.badWords = [
+let badWords = [
   "vagina",
   "dick",
   "mdrchod",
@@ -71,19 +46,6 @@ global.badWords = [
   "laund",
   "loda",
   "gulambi"];
-  
-//--------------- Tip ----------------\\
-global.Tips = [
-`Type *$prefix info* for more information....`,
-`Type *$prefix settings* to commit changes in the bot.`,
-`If you got a bug or error, then please report to developer asap by *$prefix report* command.`
-]
-
-//--------------- Menu images ----------------\\
-global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
-global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
-global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
-global.menu_pic = "https://telegra.ph/file/9a0d4053450ccfb62aaad.jpg";
 
 global.message = {
     success: "✅ 𝚂𝚞𝚌𝚌𝚎𝚜𝚜! 𝙾𝚙𝚛𝚊𝚝𝚒𝚘𝚗 𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍.",
@@ -101,36 +63,29 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "𝙲𝚙'𝚜  𝙱𝚘𝚝  />  ◉", 
-  author: process.env.Author || "@ItzCp",
-  packname: process.env.PackName || "Itz Cp v2 MD",
-  myweb: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Itz Cp's Bot",
+  botname: process.env.BotName || "Queen Anya", 
+  author: process.env.Author || "@PikaBotz",
+  packname: process.env.PackName || "Queen Anya v2 MD",
+  socialLink: process.env.Web || "https://github.com/PikaBotz",
+  footer: process.env.Footer || "© Queen Anya Bot",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Itz-Cp",
-  ownernumber: process.env.Owner_Number || "94765665354",
-  adress: process.env.Continent || "Asia, India, Assam",
-  timezone: process.env.TimeZone || "Asia/Colombo", 
+  ownername: process.env.Owner_Name || "Pika~Kun",
+  ownernumber: process.env.Owner_Number || "916900904828",
   instagramId: process.env.Insta || "8.08_only_mine",
-  email: process.env.Email_Id || "example@example.com",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "Enter",
+  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "Queen Anya", 
-  auto_read_status : process.env.AUTO_READ_STATUS || "true",
-  alwaysonline: process.env.ALWAYS_ONLINE || "true", 
-  readmessage: process.env.READ_MESSAGE || "true", 
-  auto_status_saver: process.env.AUTO_STATUS_SAVER || "true",
+  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
-  menu_pic: "https://telegra.ph/file/9a0d4053450ccfb62aaad.jpg",
   aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
   menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
+  badWords: badWords,
   message: {
     success: message.success,
     admin: message.admin,
@@ -146,3 +101,36 @@ module.exports = {
     banChat: message.banChat
   },
 }
+
+
+
+// Ignore them 👇🏻
+global.botname = process.env.BotName || "Queen Anya" 
+global.author = process.env.Author || "@PikaBotz" 
+global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.myweb = process.env.Web || "https://github.com/PikaBotz" 
+global.footer = process.env.Footer || "© Queen Anya Bot" 
+global.prefa = process.env.Prefix || ['-'] 
+global.themeemoji = process.env.ThemeEmoji || "🎐" 
+global.ownername = process.env.Owner_Name || "Pika~Kun" 
+global.ownernumber = process.env.Owner_Number || "916900904828" 
+global.adress = process.env.Continent || "Asia, India, Assam" 
+global.timezone = process.env.TimeZone || "Asia/Kolkata" 
+global.instagramId = process.env.Insta || "8.08_only_mine" 
+global.email = process.env.Email_Id || "example@example.com" 
+  
+//--------------- Tip ----------------\\
+global.Tips = [
+`Type *$prefix info* for more information....`,
+`Type *$prefix settings* to commit changes in the bot.`,
+`If you got a bug or error, then please report to developer asap by *$prefix report* command.`
+]
+
+//--------------- Menu images ----------------\\
+global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
+global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
+global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
+global.menu_pic = "https://i.ibb.co/PhDcZTM/Thumbnail.png";
+
+
+
